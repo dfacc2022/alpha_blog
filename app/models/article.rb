c551e3900cb10a436940fc6c9c80a51121b0a7eb
dfacc2022 <bcamilowdp@gmail.com>
2023-03-05 19:59:22 +0800

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,3 +3,4 @@ class Article < ActiveRecord::Base  # (<) is read as subclass from
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 3, maximum: 50}
 end
+
